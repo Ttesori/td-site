7cd5a04e0ec78fc1c869674b68a41e173b48da88
Toni Tesori <tt@tonitesori.dev>
2025-04-25 14:27:25 -0600

    Header and nav work

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,11 +4,12 @@ import lightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
 export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("public");
-  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("src/img");
   //eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("src/fonts");
-  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPlugin(lightningCSS);
+  eleventyConfig.addWatchTarget("src/js");
 
   return {
     htmlTemplateEngine: "njk"
