8d4078237037c2a4de64205e78d8250c79e33426
Toni Tesori <tt@tonitesori.dev>
2025-05-03 13:58:27 -0600

    Styling tweaks

--- eleventy.config.js
+++ eleventy.config.js
@@ -23,6 +23,7 @@ export default async function (eleventyConfig) {
     }
     return content.slice(0, length) + (useEllipsis ? "…" : "");
   });
+  eleventyConfig.addGlobalData('layout', 'layouts/base.html');
 
 
   return {
